47caa7a63d12275c895f6536430c25fc09066592
Abir Santra <santraabir8@gmail.com>
2023-01-07 00:54:17 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Navbar from "./components/NavBar/Navbar";
+import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
 	return (
 		<div className="App">
+			<Navbar />
 			<Header />
+			<Homepage />
+			<Footer />
 		</div>
 	);
 }
